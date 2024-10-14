77d1f7fda9ab0317ec486c5b8547ab1fa7ba7d4c
koba-e964 <3303362+koba-e964@users.noreply.github.com>
2024-10-14 22:11:32 +0900

    Display sfen, make custom initial position possible

--- www/src/index.tsx
+++ www/src/index.tsx
@@ -1,26 +1,45 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Head from "next/head";
 import { createRoot } from "react-dom/client";
 import { ShogiPlayer } from "shogi-player-webcomponents";
-import { createComponent, ReactWebComponent } from "@lit/react";
+import { createComponent } from "@lit/react";
 
-function hello(name: string): string {
-  return `Hello, ${name}!`;
-}
+type PlayerUpdateEvent = {
+  sfen: string;
+};
 
 function Home({}): JSX.Element {
-  const SP: ReactWebComponent<ShogiPlayer, {}> = createComponent({
+  // https://www.ai-shift.co.jp/techblog/3927
+  const SP = createComponent({
     tagName: "shogi-player",
     elementClass: ShogiPlayer,
     react: React,
+    events: {
+      update: "update",
+    },
+  });
+  const [sfen, setSfen] = React.useState(
+    "4k4/9/9/9/9/9/9/9/9 b 2r2b4g4s4n4l18p 1"
+  );
+  useEffect(() => {
+    const dom = document.getElementById("shogi-player");
+    // You need to set sfen after the component is mounted.
+    // For example, if you give an sfen string to the component directly, it will not be reflected.
+    dom?.setAttribute("sfen", sfen);
   });
   return (
     <div>
       <Head>
-        <title>Home</title>
+        <title>詰将棋 Web ソルバー</title>
       </Head>
-      <h1>{hello("shogi-board")}</h1>
-      <SP />
+      <SP
+        id="shogi-player"
+        mode="edit"
+        update={(e: CustomEvent<PlayerUpdateEvent>) => {
+          setSfen(e.detail.sfen);
+        }}
+      />
+      SFEN: <textarea id="sfen" readOnly value={sfen} rows={1} cols={80} />
     </div>
   );
 }
